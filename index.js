0a1ae0794768d87bcdf165ee3846f8c9c85732f6
petersolimine <16181699+petersolimine@users.noreply.github.com>
2022-09-17 21:18:36 -0700

    progress, but this is going to be messy.

--- index.js
+++ index.js
@@ -10,21 +10,29 @@ const initialize = async () => {
       executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", // local chrome on macOS. Remove this line of not on MacOS
       headless: false,
       ignoreHTTPSErrors: false,
+      blockAds: false,
     });
     const page = await browser.newPage();
     page.setDefaultNavigationTimeout(30000); //30 seconds
 
-    await page.goto("https://genius.com/Wilderado-surefire-lyrics");
-    // await page.waitForXPath("/html/body/div[1]/main/div[2]/div[2]/div[2]/div/div[2]");
-    let elHandle = await page.$x("/html/body/div[1]/main/div[2]/div[2]/div[2]/div/div[2]"); //try them all until error? make sure content is lyrics?
-    let elHandle2 = await page.$x("/html/body/div[1]/main/div[2]/div[2]/div[2]/div/div[5]");
-    let value = await page.evaluate((el) => el.innerText, elHandle[0]);
+    await page.goto("https://genius.com/Bob-dylan-lily-rosemary-and-the-jack-of-hearts-lyrics");
+    // hypothesis: all lyrics are split in this way.. Maybe?
 
-    //const extractedText = await page.$eval("*", (el) => el.innerText);
-    // console.log(extractedText);
+    let elHandle = await page.$x("/html/body/div[1]/main/div[2]/div[2]/div[2]/div/div[2]");
+    let value = await page.evaluate((el) => el.innerText, elHandle[0]);
+    // Some crap in every lyric
+    value = value.replace("JID “Dance Now' Official Lyrics & Meaning | Verified", "");
+    value = value.replace(/(?:\n\n)/g, "");
 
     console.log(value);
-    //s = value.replace(/([A-Z])/g, "\n$1").trim();
+    let value2 = "";
+    try {
+      let elHandle2 = await page.$x("/html/body/div[1]/main/div[2]/div[2]/div[2]/div/div[5]");
+      value2 = await page.evaluate((el) => el.innerText, elHandle2[0]);
+      console.log(value2);
+    } catch (err) {}
+
+    fs.writeFileSync("./lyrics.txt", value + value2, "UTF-8");
   } catch (err) {
     console.log("ERR:", err.message);
   }
