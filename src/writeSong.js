5f9f8ad5feb46dd5bb45765a2c542c1f95bcf92b
petersolimine <16181699+petersolimine@users.noreply.github.com>
2022-09-20 20:15:09 -0700

    fix bugs

--- src/writeSong.js
+++ src/writeSong.js
@@ -1,4 +1,4 @@
-const { Configuration, OpenAIApi } = require("openai");
+import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
